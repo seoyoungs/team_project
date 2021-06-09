abe129ded898c2227dda6dbb0431d183cd18f37e
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-06-09 15:53:13 +0900

    Add files via upload

---
+++ one_hot/c_storage.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* function declaration */
+void func(void);
+
+static int count = 5; /* global variable */ 
+
+main() { 
+
+    while (count--) {
+        func();
+    }
+    return 0;
+}
+
+void func(void) {
+
+    static int i = 5;
+    i++;
+
+    printf("i is %d and count is %d\n", i, count);
+}
